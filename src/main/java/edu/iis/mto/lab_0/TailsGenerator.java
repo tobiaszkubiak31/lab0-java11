57a0e34a63a53a8e6b1ab502ebe6fd0eff45bea8
tobiasz.kubiak <tobiaszkubiak31@gmail.com>
2020-03-02 18:21:37 +0100

    Implement TailsGenerator

--- src/main/java/edu/iis/mto/lab_0/TailsGenerator.java
+++ src/main/java/edu/iis/mto/lab_0/TailsGenerator.java
@@ -2,11 +2,21 @@ package edu.iis.mto.lab_0;
 
 import java.util.ArrayList;
 import java.util.List;
-
+// Detach tails algorithm
 public class TailsGenerator {
 
+    List<String> generatedTails;
+
     public List<String> tails(String value) {
-        return new ArrayList<>(value.length());
+
+        generatedTails = new ArrayList<>(value.length());
+
+        for (int i = 0; i <= value.length(); i++) {
+            generatedTails.add(value.substring(i));
+
+        }
+
+        return generatedTails;
     }
 
 }
